d2c2cc853391def6fb923c7895d90d05ab2213a1
Jonathan David Page <jonathan@sleepingcyb.org>
2019-05-15 20:21:02 -0400

    Fix for error at shell start

--- .config/dotfiles/rc.bash
+++ .config/dotfiles/rc.bash
@@ -1,7 +1,7 @@
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 if [ -f /usr/share/git-core/contrib/completion/git-prompt.sh ]; then
-	unalias __git_ps1
+	unalias __git_ps1 2>/dev/null
 	. /usr/share/git-core/contrib/completion/git-prompt.sh
 else
 	alias __git_ps1='echo -n >/dev/null'
