c0693f8be215fe3f33cf259ac249c03fc838f062
Jonathan David Page <jonathan@sleepingcyb.org>
2019-06-05 07:07:02 -0400

    [fish] Add fortunes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,3 +17,37 @@ source /home/jdpage/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 # go stuff
 set -x GOPATH "$HOME/go"
 set -x PATH "$GOPATH/bin" $PATH
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate 'ε'
+set __fish_git_prompt_char_stagedstate 'δ'
+set __fish_git_prompt_char_untrackedfiles '?'
+set __fish_git_prompt_char_stashstate 'z'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+function fish_prompt --description 'Write out the prompt'
+    printf '%s@%s %s%s%s%s> ' \
+    $USER (prompt_hostname) \
+    (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) \
+    (__fish_git_prompt)
+end
+
+function fish_greeting
+    if type -q fortune
+        if type -q cowsay
+            fortune -n 500 -s -a | cowsay -n
+        else
+            fortune -n 500
+        end
+    end
+end
