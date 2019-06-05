791228856b9f9644443bd0f3b102b719fecfa588
Jonathan David Page <jonathan@sleepingcyb.org>
2019-06-05 06:31:32 -0400

    Remove unneeded files

--- .config/dotfiles/emacs/bootstrap.el
+++
@@ -1,8 +0,0 @@
-;; bootstrap.el
-;; Use this as init.el -- it will automatically replace itself on first run.
-
-(require 'org)
-(find-file (concat user-emacs-directory "init.org"))
-(org-babel-tangle)
-(load-file (concat user-emacs-directory "init.el"))
-(byte-compile-file (concat user-emacs-directory "init.el"))
