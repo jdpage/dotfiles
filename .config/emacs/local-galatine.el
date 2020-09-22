f356ed397aa8bd99659e36e6b79f53dc89198d56
Jonathan David Page <jonathan@sleepingcyb.org>
2020-09-22 16:47:36 -0400

    Add galatine config

---
+++ .config/emacs/local-galatine.el
@@ -0,0 +1,7 @@
+;; Machine-local Emacs config
+
+(setq projectile-project-search-path '("~/Source"))
+
+(setq treemacs-python-executable (executable-find "python"))
+(setq elpy-rpc-python-command (executable-find "python"))
+(setq flycheck-python-pycompile-executable (executable-find "python"))
